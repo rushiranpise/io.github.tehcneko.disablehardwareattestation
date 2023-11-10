952148750a831bcf7739b34f4d2fed4a53f17bca
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-11-10 11:09:50 +0530

    fix safteynet

--- app/src/main/java/io/github/tehcneko/disablehardwareattestation/GMSHook.java
+++ app/src/main/java/io/github/tehcneko/disablehardwareattestation/GMSHook.java
@@ -23,8 +23,8 @@ public class GMSHook implements IXposedHookLoadPackage {
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
     private static final String PROVIDER_NAME = "AndroidKeyStore";
-    private static boolean sIsGms = false;
-    private static boolean sIsFinsky = false;
+    private static volatile boolean sIsGms = false;
+    private static volatile boolean sIsFinsky = false;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
@@ -33,9 +33,11 @@ public class GMSHook implements IXposedHookLoadPackage {
             sIsGms = true;
             spoofBuildGms();
         }
+        
         if (PACKAGE_FINSKY.equals(loadPackageParam.packageName)) {
             sIsFinsky = true;
         }
+        
         if (sIsGms || sIsFinsky) {
             try {
                 KeyStore keyStore = KeyStore.getInstance(PROVIDER_NAME);
@@ -44,7 +46,9 @@ public class GMSHook implements IXposedHookLoadPackage {
                 XposedHelpers.findAndHookMethod(keyStoreSpi.get(keyStore).getClass(), "engineGetCertificateChain", String.class, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) {
+                        // Check stack for SafetyNet or Play Integrity
                         if (isCallerSafetyNet() || sIsFinsky) {
+                            Log.i(TAG, "Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
                             param.setThrowable(new UnsupportedOperationException());
                         }
                     }
@@ -57,11 +61,21 @@ public class GMSHook implements IXposedHookLoadPackage {
     }
 
     private static void spoofBuildGms() {
-        // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setBuildField("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-        setBuildField("PRODUCT", "marlin");
-        setBuildField("DEVICE", "marlin");
-        setBuildField("MODEL", "Pixel XL");
+        // Alter build parameters to ASUS_X00HD for avoiding hardware attestation enforcement
+        setPropValue("PRODUCT", "WW_Phone");
+        setPropValue("PRODUCT_FOR_ATTESTATION", "WW_Phone");
+        setPropValue("DEVICE", "ASUS_X00HD_4");
+        setPropValue("DEVICE_FOR_ATTESTATION", "ASUS_X00HD_4");
+        setPropValue("MANUFACTURER", "Asus");
+        setPropValue("MANUFACTURER_FOR_ATTESTATION", "Asus");
+        setPropValue("BRAND", "Asus");
+        setPropValue("BRAND_FOR_ATTESTATION", "Asus");
+        setPropValue("MODEL", "ASUS_X00HD");
+        setPropValue("MODEL_FOR_ATTESTATION", "ASUS_X00HD");
+        setPropValue("FINGERPRINT", "asus/WW_Phone/ASUS_X00HD_4:7.1.1/NMF26F/14.2016.1801.372-20180119:user/release-keys");
+        setPropValue("ID", "NMF26F");
+        setPropValue("TYPE", "user");
+        setPropValue("TAGS", "release-keys");
         setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
     }
 
@@ -70,32 +84,35 @@ public class GMSHook implements IXposedHookLoadPackage {
                 .anyMatch(elem -> elem.getClassName().contains("DroidGuard"));
     }
 
-    private static void setBuildField(String key, String value) {
+    private static void setPropValue(String key, Object value) {
         try {
-            // Unlock
             Field field = Build.class.getDeclaredField(key);
             field.setAccessible(true);
-            // Edit
             field.set(null, value);
-            // Lock
             field.setAccessible(false);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            Log.e(TAG, "Failed to spoof Build." + key, e);
+            Log.e(TAG, "Failed to set prop " + key, e);
         }
     }
-
-    private static void setVersionField(String key, Integer value) {
+    private static void setVersionField(String key, Object value) {
         try {
-            // Unlock
             Field field = Build.VERSION.class.getDeclaredField(key);
             field.setAccessible(true);
-            // Edit
             field.set(null, value);
-            // Lock
             field.setAccessible(false);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            Log.e(TAG, "Failed to spoof Build." + key, e);
+            Log.e(TAG, "Failed to set version field " + key, e);
         }
     }
 
+    private static void setVersionFieldString(String key, String value) {
+        try {
+            Field field = Build.VERSION.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
 }
